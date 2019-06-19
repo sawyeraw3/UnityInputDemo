18dd561d2f294873d383634c2d4cd1e29e0e0b83
Sawyer Warden <sawyer.warden@gmail.com>
2019-06-18 23:23:21 -0400

    uploading project

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,441 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/PlayerControls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class PlayerControls : IInputActionCollection
+{
+    private InputActionAsset asset;
+    public PlayerControls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""PlayerControls"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""dd17661c-b24f-4354-855a-4bc9ec4a6abc"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""id"": ""74c7c591-d843-4374-9786-d938b1586068"",
+                    ""expectedControlLayout"": ""Vector2"",
+                    ""continuous"": true,
+                    ""passThrough"": false,
+                    ""initialStateCheck"": false,
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""bindings"": []
+                },
+                {
+                    ""name"": ""Look"",
+                    ""id"": ""963c2c2a-3594-4aaf-9156-3addf4ab045d"",
+                    ""expectedControlLayout"": ""Vector2"",
+                    ""continuous"": true,
+                    ""passThrough"": false,
+                    ""initialStateCheck"": false,
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""bindings"": []
+                },
+                {
+                    ""name"": ""ActionButtons"",
+                    ""id"": ""5a036634-86d8-44e7-b037-a4360175ad8c"",
+                    ""expectedControlLayout"": ""Button"",
+                    ""continuous"": true,
+                    ""passThrough"": false,
+                    ""initialStateCheck"": false,
+                    ""processors"": """",
+                    ""interactions"": ""Press"",
+                    ""bindings"": []
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""0e5be69d-c240-488e-8d02-60e171cad0cb"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""WASD"",
+                    ""id"": ""011487a4-a4ab-4957-9aa1-399d81b6c423"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""3f2fa28a-0229-4a45-bf70-648ae91d6f1c"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""5e168331-f591-4413-b0e5-d6912fd1f0e2"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""5e44d9b9-9ba2-45e3-a628-3e71d804cab8"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""61e379b2-3129-431d-ae16-d354c3c59d01"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9566c7b0-1fc1-45d6-99ca-3c4324f11a2d"",
+                    ""path"": ""<Gamepad>/rightStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""Mouse"",
+                    ""id"": ""17fb3ef5-c5e5-4a4e-9fcb-4d5964ff0cc8"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""a4e02bb6-5b0e-4b97-b5b6-a5b1220db028"",
+                    ""path"": ""<Mouse>/position/y"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""e22ee29b-e575-4028-9040-cff6a86bb236"",
+                    ""path"": ""<Mouse>/position/y"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""3ec1e04e-2f0f-4423-8812-9004d5af409b"",
+                    ""path"": ""<Mouse>/position/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""9bab674d-29ac-4917-9707-91aaa3b3556e"",
+                    ""path"": ""<Mouse>/position/x"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""Arrows"",
+                    ""id"": ""3744c5ff-73dc-4fe9-aa2f-9acf0f202fc6"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""5074d82e-607a-4333-acb8-9753b2e3841c"",
+                    ""path"": ""<Keyboard>/upArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""e08c919f-b965-44f2-86b9-515012cba960"",
+                    ""path"": ""<Keyboard>/downArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""03a3a442-f71d-4fd9-ae1c-8fcf7c5be8e1"",
+                    ""path"": ""<Keyboard>/leftArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""f0497229-3382-48cd-b28d-e2cc24cc383f"",
+                    ""path"": ""<Keyboard>/rightArrow"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""398943a9-3bb3-4f8c-8c3f-7ac1798b2cc8"",
+                    ""path"": ""<Gamepad>/buttonEast"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ActionButtons"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""bd81db74-985b-4c67-bcc9-dfa9646f54d1"",
+                    ""path"": ""<Gamepad>/buttonNorth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ActionButtons"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""cfc4acfe-acfa-44fe-b778-ee66f5f9dc10"",
+                    ""path"": ""<Gamepad>/buttonSouth"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ActionButtons"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""f4c33b63-33b6-486d-a651-b5b3520d58bc"",
+                    ""path"": ""<Gamepad>/buttonWest"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ActionButtons"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c7fc9bf8-82e5-494b-b97b-eb5d29708311"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""ActionButtons"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false,
+                    ""modifiers"": """"
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.GetActionMap("Player");
+        m_Player_Move = m_Player.GetAction("Move");
+        m_Player_Look = m_Player.GetAction("Look");
+        m_Player_ActionButtons = m_Player.GetAction("ActionButtons");
+    }
+
+    ~PlayerControls()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes
+    {
+        get => asset.controlSchemes;
+    }
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private InputAction m_Player_Move;
+    private InputAction m_Player_Look;
+    private InputAction m_Player_ActionButtons;
+    public struct PlayerActions
+    {
+        private PlayerControls m_Wrapper;
+        public PlayerActions(PlayerControls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move { get { return m_Wrapper.m_Player_Move; } }
+        public InputAction @Look { get { return m_Wrapper.m_Player_Look; } }
+        public InputAction @ActionButtons { get { return m_Wrapper.m_Player_ActionButtons; } }
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled { get { return Get().enabled; } }
+        public InputActionMap Clone() { return Get().Clone(); }
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                Move.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                Move.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                Move.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnMove;
+                Look.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                Look.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                Look.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnLook;
+                ActionButtons.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnActionButtons;
+                ActionButtons.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnActionButtons;
+                ActionButtons.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnActionButtons;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                Move.started += instance.OnMove;
+                Move.performed += instance.OnMove;
+                Move.canceled += instance.OnMove;
+                Look.started += instance.OnLook;
+                Look.performed += instance.OnLook;
+                Look.canceled += instance.OnLook;
+                ActionButtons.started += instance.OnActionButtons;
+                ActionButtons.performed += instance.OnActionButtons;
+                ActionButtons.canceled += instance.OnActionButtons;
+            }
+        }
+    }
+    public PlayerActions @Player
+    {
+        get
+        {
+            return new PlayerActions(this);
+        }
+    }
+    public interface IPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
+        void OnActionButtons(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerMovementManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class PlayerMovementManager : MonoBehaviour
+{
+
+		public Camera _camera;
+		public float _clampMin = -10f;
+		public float _clampMax = 15f;
+
+		public Transform gun;
+		public float _moveSpeed;
+		public float _lookSpeed;
+
+		private PlayerControls _controls;
+    private Vector2 _movementAxis;
+    private Vector2 _lookAxis;
+    private Vector2 _lookRot = Vector2.zero;
+
+    private void OnEnable()
+    {
+    	_controls = new PlayerControls();
+    	_controls.Player.Move.performed += HandleMovement;
+    	_controls.Player.Move.Enable();
+    	_controls.Player.Look.performed += HandleLook;
+    	_controls.Player.Look.Enable();
+    	_controls.Player.ActionButtons.performed += HandleActionButtons;
+    	_controls.Player.ActionButtons.Enable();
+    }
+
+    private void OnDisable()
+    {
+    	_controls.Player.Move.performed -= HandleMovement;
+    	_controls.Player.Move.Disable();
+    	_controls.Player.Look.performed -= HandleLook;
+    	_controls.Player.Look.Disable();
+    	_controls.Player.ActionButtons.performed -= HandleActionButtons;
+    	_controls.Player.ActionButtons.Disable();
+    }
+
+    private void Update()
+    {
+    	if (Mathf.Abs(_movementAxis.x) > .3 || Mathf.Abs(_movementAxis.y) > .3)
+    	{
+    		transform.position += _movementAxis.x * Time.deltaTime * _moveSpeed * Vector3.ProjectOnPlane(_camera.transform.right, Vector3.up);
+    		transform.position += _movementAxis.y * Time.deltaTime * _moveSpeed * Vector3.ProjectOnPlane(_camera.transform.forward, Vector3.up);
+
+    		// Necessary as action only triggered on press
+    		_movementAxis = Vector2.zero;
+    	}
+    }
+
+    private void HandleMovement(InputAction.CallbackContext c)
+    {
+    	_movementAxis = c.ReadValue<Vector2>();
+    }
+
+    private void HandleLook(InputAction.CallbackContext c)
+    {
+    	_lookAxis = c.ReadValue<Vector2>();
+    	_lookRot.x += - _lookAxis.y;
+    	_lookRot.y += _lookAxis.x;
+    	_lookRot.x = Mathf.Clamp(_lookRot.x, _clampMin, _clampMax);
+    	transform.eulerAngles = new Vector2(0, _lookRot.y) * _lookSpeed;
+    	_camera.transform.localRotation = Quaternion.Euler(_lookRot.x * _lookSpeed, 0, 0);
+
+    	// Adjust angle of gun object
+    	gun.up = _camera.transform.forward;
+    }
+
+  	private void HandleActionButtons(InputAction.CallbackContext c)
+  	{
+  		string key = c.control.name;
+
+  		// Lame switches...
+    	switch(key)
+    	{
+    		case "space":
+    			GetComponent<Rigidbody>().AddForce(transform.up * 100f);
+    			break;
+    		case "buttonSouth":
+    			GetComponent<Rigidbody>().AddForce(transform.up * 100f);
+    			break;
+    		default:
+    			Debug.Log(key);
+    			break;
+    	}
+  	}
+}
